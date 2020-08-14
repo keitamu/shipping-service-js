324297df93f0f41550ca507021121eb58bf2f6b0
keitamu <41756721+keitamu@users.noreply.github.com>
2020-08-15 01:18:04 +0900

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@
 const express = require('express')
 const app = express()
 
+app.get('/', (request, response) => {
+  response.send({ weightLB: 5 })
+})
+
 
 app.get('/products/:productId', (request, response) => {
   response.send({ weightLB: 50 })
- 
-
 })
 
 let PORT = process.env.PORT || 3001;
